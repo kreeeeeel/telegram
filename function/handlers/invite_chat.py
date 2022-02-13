af468470422644bd8075932b963f741dc83ece76
Kirill Nemtyrev <75456383+KirillNemtyrev@users.noreply.github.com>
2022-02-14 00:46:20 +0300

    Update invite_chat.py

--- function/handlers/invite_chat.py
+++ function/handlers/invite_chat.py
@@ -11,25 +11,21 @@ data = json.loads(config.read())
 config.close()
 
 @dp.message_handler(content_types=["new_chat_members"])
-async def new_chat_handler(message):
+async def new_chat_handler(message: types.Message):
     try:
-        if message.new_chat_members[0].id != bot.id:
-            return 
+        if message.new_chat_members[0].id == bot.id:
+            if not GetDataFromChat.is_created_chat(message.chat.id):
+                GetDataFromChat.created_data_chat(message.chat.id)
+                
+            caption = data["emojio"] + f" Приветствую Вас!\nМое имя -  *{data['name_rus']}*\n"
+            caption += "_Я игровой бот и для запуска моей работы мне требуются права администратора :)_\n"
+            caption += 'После выдачи прав, нажмите кнопку *Проверить*'
 
-        if not GetDataFromChat.is_created_chat(message.chat.id):
-            GetDataFromChat.created_data_chat(message.chat.id)
-            
-        caption = data["emojio"] + f" Приветствую вас, моё имя *{data['name_rus']}*\n"
-        caption += "Я вроде как считаюсь игровый ботом\n"
-        caption += 'Для запуск функционала бота требуются:\n'
-        caption += '📌 _Права администратора_\n\n'
-        caption += 'После выдачи прав, нажмите кнопку *Проверить*'
+            buttons  = [types.InlineKeyboardButton(text='Проверить', callback_data="Проверить")] 
+            keyboard = types.InlineKeyboardMarkup(row_width=1)
+            keyboard.add(*buttons)
 
-        buttons  = [types.InlineKeyboardButton(text='Проверить', callback_data="Проверить")] 
-        keyboard = types.InlineKeyboardMarkup(row_width=1)
-        keyboard.add(*buttons)
-
-        return await bot.send_message(chat_id=message.chat.id, text=caption, reply_markup=keyboard)
+            return await message.answer(text=caption, reply_markup=keyboard)
 
     except Exception as e:
         logging.error(e, exc_info=True)
