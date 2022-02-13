858644c890a415386ea6b76fdacaffafbe7ba670
Kirill Nemtyrev <75456383+KirillNemtyrev@users.noreply.github.com>
2022-02-13 23:06:45 +0300

    Update invite_chat.py

--- function/handlers/invite_chat.py
+++ function/handlers/invite_chat.py
@@ -4,16 +4,20 @@ import logging
 
 from aiogram import types
 from dispatcher import dp, bot
+from classes import GetDataFromChat
 
 config = open(os.getcwd() + "/config.json", encoding="UTF-8")
 data = json.loads(config.read())
 config.close()
 
 @dp.message_handler(content_types=["new_chat_members"])
-async def new_chat_handler(message: types.Message):
+async def new_chat_handler(message):
     try:
         if message.new_chat_members[0].id != bot.id:
             return 
+
+        if not GetDataFromChat.is_created_chat(message.chat.id):
+            GetDataFromChat.created_data_chat(message.chat.id)
             
         caption = data["emojio"] + f" Приветствую вас, моё имя *{data['name_rus']}*\n"
         caption += "Я вроде как считаюсь игровый ботом\n"
@@ -25,7 +29,7 @@ async def new_chat_handler(message: types.Message):
         keyboard = types.InlineKeyboardMarkup(row_width=1)
         keyboard.add(*buttons)
 
-        return await message.answer(text=caption, reply_markup=keyboard)
+        return await bot.send_message(chat_id=message.chat.id, text=caption, reply_markup=keyboard)
 
     except Exception as e:
         logging.error(e, exc_info=True)
