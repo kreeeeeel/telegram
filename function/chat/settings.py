f2516d80b81956ec7cbac38e276e957ec7c5e54c
Kirill Nemtyrev <75456383+KirillNemtyrev@users.noreply.github.com>
2022-02-14 17:31:37 +0300

    Update settings.py

--- function/chat/settings.py
+++ function/chat/settings.py
@@ -33,7 +33,7 @@ def get_settings(chat_id, full_name):
     chat = GetDataFromChat.export_data_from_chat(chat=chat_id)
 
     caption = data["emojio"] + f" Настройки беседы: {full_name}\n\n"
-    caption += f'Упоминание игроков: {( "Выключено ❌" , "Включено ✔" )[ chat["pin_user"] ]}\n'
+    caption += f'Упоминание: {( "Выключено ❌" , "Включено ✔" )[ chat["pin_user"] ]}\n'
     caption += f'Анти КАПС: {( "Выключено ❌" , "Включено ✔" )[ chat["anti_capslock"] ]}\n'
     caption += f'Анти URL: {( "Выключено ❌" , "Включено ✔" )[ chat["anti_url"] ]}\n'
 
