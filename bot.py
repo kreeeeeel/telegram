8597876c48aeebda94b3206362005b4d881cea86
Kirill Nemtyrev <75456383+KirillNemtyrev@users.noreply.github.com>
2022-02-24 15:31:30 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,21 @@
-from aiogram import executor
-from dispatcher import dp
-import function
-import os
+from aiogram import Bot, Dispatcher, executor
+import asyncio
+from datetime import datetime
+from datetime import timezone
+from pytz import timezone
 
-if __name__ == "__main__":
-    if not os.path.isdir("data"):
-        os.mkdir("data")
+neketa = 237408360
+bot = Bot(token="1947165848:AAF5FJxUmxPJNDa4KJxkFdIol-Q-SUI_TGo")
+dp = Dispatcher(bot)
 
-    if not os.path.isdir("data/chats"):
-        os.mkdir("data/chats")
+async def on_startup(_):
+    date = datetime.now(timezone('Europe/Moscow'))
+    if date.hour == 9 and date.minute == 30:
+        await bot.send_message(chat_id=neketa, text='Кирюша пидор')
 
-    if not os.path.isdir("data/users"):
-        os.mkdir("data/users")
+    await asyncio.sleep(30)
+    await on_startup(_)
 
-    executor.start_polling(dp, skip_updates=True)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=False, on_startup=on_startup)
