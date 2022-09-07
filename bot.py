f9169796748ff697aebde5f31b0023d20d5939b3
Kirill Nemtyrev <75456383+KirillNemtyrev@users.noreply.github.com>
2022-09-07 16:58:39 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,13 @@
-from aiogram import Bot, Dispatcher, executor
-import asyncio
+from aiogram import executor
 from dispatcher import dp
+import function
+import os
 
-
-if __name__ == '__main__':
-
-    if not os.isdir
-    executor.start_polling(dp, skip_updates=False, on_startup=on_startup)
+if __name__ == "__main__":
+    if not os.path.isdir("data"):
+        os.mkdir("data")
+        
+    if not os.path.isdir("data/chats"):
+        os.mkdir("data/chats")
+        
+    executor.start_polling(dp, skip_updates=True)
