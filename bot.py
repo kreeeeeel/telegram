d40e52f13ffd2c8c3b4c7995e33c5de541521b7d
Kirill Nemtyrev <75456383+KirillNemtyrev@users.noreply.github.com>
2022-09-07 16:59:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,4 +10,7 @@ if __name__ == "__main__":
     if not os.path.isdir("data/chats"):
         os.mkdir("data/chats")
         
+    if not os.path.isdir("data/users"):
+        os.mkdir("data/users")
+        
     executor.start_polling(dp, skip_updates=True)
